1cd22c290d0bf22c645b078e671d9ecc60b22c20
Angel Resendiz <angel.resendiz@gmail.com>
2021-03-08 10:12:18 -0600

    [ADD] Button re-enabled delay to prevent double-click errors

--- components/ResponseButtons/ResponseButtons.js
+++ components/ResponseButtons/ResponseButtons.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Button } from "components";
 
 import styles from "./ResponseButtons.module.sass";
 
 const ResponseButtons = ({ onResponse = () => {} }) => {
+  const [buttonsEnabled, setButtonsEnabled] = useState(true);
+
+  // Delay to prevent double-click errors
+  const handleClick = (val) => {
+    if (buttonsEnabled) {
+      setButtonsEnabled(false);
+      onResponse(val);
+      setTimeout(() => setButtonsEnabled(true), 500);
+    }
+  };
+
   return (
     <div className={styles.responseButtons}>
-      <Button secondary onClick={() => onResponse("True")}>
+      <Button secondary onClick={() => handleClick("True")}>
         True
       </Button>
-      <Button primary onClick={() => onResponse("False")}>
+      <Button primary onClick={() => handleClick("False")}>
         False
       </Button>
     </div>
--- layouts/Quiz/Quiz.js
+++ layouts/Quiz/Quiz.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useSelector, useDispatch } from "react-redux";
 import { ClockLoader } from "react-spinners";
